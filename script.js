43ee9c86bb2f55757ddcbeed0c87ddb8d16aefa0
GabrielDSanta <eloviveiros@hotmail.com>
2022-08-25 23:13:55 -0300

    Calculadora V2

--- script.js
+++ script.js
@@ -4,3 +4,8 @@
 let numberA = prompt("Digite o primeiro valor")
 let numberB = prompt("Digite o segundo valor")
 
+function Somar(numberA, numberB){
+    return parseInt(numberA) + parseInt(numberB)
+}
+
+console.log(Somar(numberA, numberB))
